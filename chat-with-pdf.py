168891610c7742232172842513b68268bad76efa
Digiotai2025 <146609374+Digiotai2025@users.noreply.github.com>
2023-10-02 12:07:11 +0530

    Update chat-with-pdf.py

--- chat-with-pdf.py
+++ chat-with-pdf.py
@@ -194,11 +194,11 @@ html_css_login = """
 """
 html_button_login = (
     html_css_login
-    + f"<a href='{login_link}' class='button-login' target='_blank'>Log In</a>"
+    + f"<a href='{login_link}' class='button-login' target='_parent'>Log In</a>"
 )
 html_button_logout = (
     html_css_login
-    + f"<a href='{logout_link}' class='button-login' target='_blank'>Log Out</a>"
+    + f"<a href='{logout_link}' class='button-login' target='_parent'>Log Out</a>"
 )
 def button_login():
     """
